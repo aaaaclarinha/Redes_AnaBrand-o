55b3c123473e64bd7b04e467871a590d8a140bd0
Ana Clara Santos Brandão <ana220069@ilum.cnpem.br>
2023-03-09 17:53:24 -0300

    Aula 2 - 09.03.23

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,37 @@
- 
+def gene_cb():
+    """ gera um gene valido para cada caixa binária
+    
+        return:
+            valor de 0 ou 1         
+    """
+    
+    lista = [0,1]
+    gene = random.choice(lista)
+    return gene 
+
+def ind_cb(n):
+    """ gera o indivíuo 
+    
+        arg:
+            n: número de genes do indivíduo 
+            
+        return:
+            uma lista com n genes
+    """
+    
+    ind = []
+    for i in range(n):
+        gene = gene_cb()
+        ind.append(gene)
+    return ind
+
+def fun_obj_(individuo):
+    """ soma os valores dos genes 
+    
+        arg:
+            individuo: lista contendo as caixas binárias 
+            
+        return
+            valor da soma 
+    """
+    return sum(individuo) 
